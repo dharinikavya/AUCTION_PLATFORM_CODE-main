e5b5c2ee640c88400f44254ba26cab63c61d3d2a
Dharini Kavya <dhakav1809@gmail.com>
2025-12-11 13:10:34 +0530

    Updated API endpoints to Render backend

--- Frontend/src/utils/APIs.js
+++ Frontend/src/utils/APIs.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://auction-backend-v4ox.onrender.com/api/v1";
-export const USER_API_POINT="http://localhost:5100/api/v1/user"
-export const AUCTION_API_POINT="http://localhost:5100/api/v1/auctionitem"
-export const BID_API_POINT="http://localhost:5100/api/v1/bid"
-export const COMMISSIONPROOF_API_POINT="http://localhost:5100/api/v1/commission"
-export const SUPER_ADMIN_API_POINT="http://localhost:5100/api/v1/superadmin"
+export const USER_API_POINT = "https://auction-backend-v4ox.onrender.com/api/v1/user";
+export const AUCTION_API_POINT = "https://auction-backend-v4ox.onrender.com/api/v1/auctionitem";
+export const BID_API_POINT = "https://auction-backend-v4ox.onrender.com/api/v1/bid";
+export const COMMISSIONPROOF_API_POINT = "https://auction-backend-v4ox.onrender.com/api/v1/commission";
+export const SUPER_ADMIN_API_POINT = "https://auction-backend-v4ox.onrender.com/api/v1/superadmin";
