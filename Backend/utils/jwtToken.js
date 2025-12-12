d0b5357942811b8ea6a7f0ffc340b296b2c9f378
Dharini Kavya <dhakav1809@gmail.com>
2025-12-12 23:34:05 +0530

    re

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,21 +1,16 @@
-import jwt from "jsonwebtoken";
-
 export const genrateToken = (user, message, statusCode, res) => {
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "5d",
-  });
-
-  res.status(statusCode)
-    .cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",        // only secure in production
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 5 * 24 * 60 * 60 * 1000,
-    })
-    .json({
-      success: true,
-      message,
-      user,
-      token,
-    });
-};
+    const token = user.genrateJsonWebToken()
+    res
+      .status(statusCode)
+      .cookie("token", token, {
+        expires: new Date(Date.now() + 7 * 24 * 60 *60* 1000),
+        httpOnly:true
+      })
+      .json({
+        success: true,
+        message,
+        user,
+        token,
+      })
+  }
+  
