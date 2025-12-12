9c6b79d7fb39638ecac65e77ebe4299158dd2b92
Dharini Kavya <dhakav1809@gmail.com>
2025-12-12 23:45:14 +0530

    redo

--- Backend/app.js
+++ Backend/app.js
@@ -19,14 +19,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-frontend-domain.com"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+  }),
+)
 app.use(
   fileupload({
     useTempFiles: true,
