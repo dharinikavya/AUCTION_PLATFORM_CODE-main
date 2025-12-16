ac0b1b81218a1773b213b99ec23fe0bd11cb6907
Dharini Kavya <dhakav1809@gmail.com>
2025-12-16 18:19:58 +0530

    notification

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,20 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import cloudinary from "cloudinary";
+import { endedAuctionCron } from "./cron/endedAuctionCron.js"; // ✅ ADD THIS
 
 dotenv.config();
 
 cloudinary.v2.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 });
 
 const PORT = process.env.PORT || 5100;
+
+/* 🔥 START CRON JOB */
+endedAuctionCron();
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
